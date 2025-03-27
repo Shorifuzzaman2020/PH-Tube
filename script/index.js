4da6e659e24dedd95f906af6c41f3fef41107d74
Shorifuzzaman2020 <shorifuzzamansoil2020@gmail.com>
2025-03-27 17:00:21 +0600

    show more section done

--- script/index.js
+++ script/index.js
@@ -1,3 +1,10 @@
+function removeActiveClass(){
+    const activeButtons=document.getElementsByClassName("active");
+    for(let btn of activeButtons){
+        btn.classList.remove("active");
+    }
+}
+
 function loadCategory(){
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res)=>res.json())
@@ -6,17 +13,52 @@ function loadCategory(){
 function loadVideos(){
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res)=>res.json())
-    .then((data)=>displayVideo(data.videos))
+    .then((data)=>{
+        removeActiveClass();
+        document.getElementById("btn-all").classList.add("active");
+        displayVideo(data.videos)
+        
+    })
 }
 const loadCategoryVideos=(id)=>{
     const url=`https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
     fetch(url)
     .then((res)=>res.json())
     .then((data)=>{
+        removeActiveClass();
         const clickedButton=document.getElementById(`btn-${id}`);
         clickedButton.classList.add("active");
         displayVideo(data.category);
     })
+}
+const loadVideoDetails=(videoId)=>{
+    const url=`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        displayVideoDetails(data.video);
+    })
+}
+const displayVideoDetails=(video)=>{
+    document.getElementById("video_details").showModal();
+    const detailsContainer=document.getElementById("details-container");
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Title: <p>${video.title}</p></h2>
+    
+    <p> Author: ${video.authors[0].profile_name}</p>
+    <p> Views: ${video.others.views}</p>
+    <p> Description: ${video.description}</p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div> `
 }
 
 function displayCategory(categories){
@@ -63,8 +105,8 @@ const displayVideo=(videos)=>{
                 <p class="text-sm text-gray-400 flex gap-1 mb-3">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000" alt=""></p>
                 <p class="text-sm text-gray-400">${video.others.views} views</p>
              </div>
-             
         </div>
+             <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
         
         `;
         videoContainer.append(videoCart);
